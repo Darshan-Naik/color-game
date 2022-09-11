19b9bb5b4e969669e1754e58f7bde60d7ea5a7a6
Darshan Gajanan Naik <darshangajanannaik@Meeshos-MacBook-Pro-31.local>
2022-09-11 20:32:28 +0530

    added update

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ let colors = [
   "orange",
   "skyblue",
 ];
+let selectedColors = [];
 colors = [...colors, ...colors];
 let selected = null;
-let block = false;
+let block = true;
 let attempt = 0;
 let correct = 0;
 function randomColor() {
@@ -32,10 +33,25 @@ pallets.forEach((pallet) => {
   pallet.addEventListener("click", handlePalletClick);
 });
 
+async function start(){
+    let targets = [...pallets];
+    while(targets.length){
+        const random = Math.floor(Math.random() * targets.length);
+        const target = targets[random];
+        targets = targets.filter((item) => item !== target);
+        target.classList.remove("hide-color");
+        await new Promise((resolve) => setTimeout(resolve,250));
+        target.classList.add("hide-color");
+    }
+    block = false;
+}
+start();
+
 async function handlePalletClick({ target }) {
-  if (block || target === selected) return;
+    const color = target.getAttribute("data-color");
+    console.log(color,selectedColors);
+  if (block || target === selected || selectedColors.includes(color)) return;
   block = true;
-  const color = target.getAttribute("data-color");
   target.classList.remove("hide-color");
   if (!selected) {
     selected = target;
@@ -53,6 +69,7 @@ async function handlePalletClick({ target }) {
       );
     } else {
       correct++;
+      selectedColors.push(selectedColor);
       if (correct === 8) {
         pop.innerHTML = `You Won in ${attempt} attempts`;
         pop.classList.remove("hide");
